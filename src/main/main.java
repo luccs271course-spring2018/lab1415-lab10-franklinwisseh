20d485e586f5fadfad21038019adda1a72f6045f
FrankDozen <31441092+FrankDozen@users.noreply.github.com>
2017-12-12 21:39:19 -0600

    main DONE

--- src/main/main.java
+++ src/main/main.java
@@ -40,3 +40,55 @@ public class Main {
         GeoGraph.addEdge(ID, UT);
 
         System.out.println(GeoGraph);
+
+        final Iterator<String> bf = new BreadthFirstIterator<>(myGraph, NV);
+        while (bf.hasNext()) {
+            final String state = bf.next();
+            System.out.println("Breadth first search starts at NV " + state);
+        }
+
+        final Iterator<String> bf2 = new BreadthFirstIterator<>(myGraph, UT);
+        while (bf2.hasNext()) {
+            final String state = bf2.next();
+            System.out.println("Breadth-Two first search starts at UT " + state);
+        }
+
+        final Iterator<String> cf = new ClosestFirstIterator<>(myGraph, NV);
+        while (cf.hasNext()) {
+            final String state = cf.next();
+            System.out.println("Closest first search starts at NV " + state);
+        }
+
+        final Iterator<String> cf2 = new ClosestFirstIterator<>(myGraph, UT);
+        while (cf2.hasNext()) {
+            final String state = cf2.next();
+            System.out.println("Closest-Two first search starts at UT " + state);
+        }
+
+        final Iterator<String> df = new DepthFirstIterator<>(myGraph, NV);
+        while (df.hasNext()) {
+            final String state = df.next();
+            System.out.println("Depth first search starts at NV " + state);
+        }
+
+        final Iterator<String> df2 = new DepthFirstIterator<>(myGraph, UT);
+        while (df2.hasNext()) {
+            final String state = df2.next();
+            System.out.println("Depth-Two first search starts at UT " + state);
+        }
+
+        final Iterator<String> rw = new RandomWalkIterator<>(myGraph, NV);
+        while (rw.hasNext()) {
+            final String state = rw.next();
+            System.out.println("Closest first search starts at NV " + state);
+        }
+
+        final Iterator<String> rw = new RandomWalkIterator<>(myGraph, UT);
+        while (rw.hasNext()) {
+            final String state = rw.next();
+            System.out.println("Closest-Two first search starts at UT " + state);
+        }
+        GreedyColoring graph = new GreedyColoring(myGraph);
+        System.out.println(graph.getColoring());
+    }
+}
